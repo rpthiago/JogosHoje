7347300283f07ba9a20aaba80fe31d9b35c4be4e
rpthiago <112081890+rpthiago@users.noreply.github.com>
2022-12-12 20:34:52 -0300

    Create metodo_under_4_5.py

---
+++ App/pages/metodo_under_4_5.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+from datetime import date
+
+st.title("Jogos do Dia")
+
+dia = st.date_input(
+    "Data de Análise",
+    date.today())
+
+def load_data_jogos():
+    data_jogos = pd.read_csv("https://github.com/rpthiago/JogosHoje/blob/main/metodounder4_5.csv?raw=true")
+    
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
