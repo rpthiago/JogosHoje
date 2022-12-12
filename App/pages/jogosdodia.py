be4d56b062e1f32039a3bc3ebe027e278ff60384
rpthiago <112081890+rpthiago@users.noreply.github.com>
2022-12-12 19:15:22 -0300

    Create jogosdodia.py

---
+++ App/pages/jogosdodia.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+from datetime import date
+
+st.title("Jogos do Dia")
+
+def load_data_jogos():
+    data_jogos = pd.read_xlsx("https://github.com/rpthiago/JogosHoje/raw/main/jogos_dia_completo.xlsx?raw=true")
+    
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
